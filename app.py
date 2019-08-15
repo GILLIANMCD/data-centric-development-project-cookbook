2962a5a924233fe058b353b0da49bbc6e573ebf2
GILLIANMCD <gillian2677@gmail.com>
2019-08-15 12:37:27 +0000

    connect to Mongo

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = 'cookbook_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://<username>:<password>@myfirstcluster-eimbl.mongodb.net/test?retryWrites=true&w=majority'
 
 
 @app.route('/')
